7c239fe024b16ba92738e5ed1efe6eb87106a583
jawad44 <jawad44@iastate.edu>
2017-09-24 16:50:31 -0500

    new work

---
+++ app/src/main/java/com/example/jawad44/prepark/setupLots.java
@@ -0,0 +1,13 @@
+package com.example.jawad44.prepark;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class setupLots extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_lots);
+    }
+}
