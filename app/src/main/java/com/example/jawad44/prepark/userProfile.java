07770eb62c4c47f58264222dcc2ec2c3b25f2d35
jawad44 <jawad44@iastate.edu>
2017-09-25 16:54:07 -0500

    changes

--- app/src/main/java/com/example/jawad44/prepark/userProfile.java
+++ app/src/main/java/com/example/jawad44/prepark/userProfile.java
@@ -1,7 +1,9 @@
 package com.example.jawad44.prepark;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class userProfile extends AppCompatActivity {
@@ -11,9 +13,23 @@ public class userProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-        final Button setuplots = (Button) findViewById(R.id.button_setuplots);
-        final Button mylots = (Button) findViewById(R.id.button_mylots);
-        final Button transactions = (Button) findViewById(R.id.button_transactions);
+        final Button b_setuplots = (Button) findViewById(R.id.button_setuplots);
+        final Button b_mylots = (Button) findViewById(R.id.button_mylots);
+        final Button b_transactions = (Button) findViewById(R.id.button_transactions);
 
+        b_setuplots.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i_setuplots = new Intent(userProfile.this, setupLots.class);
+                startActivity(i_setuplots);
+            }
+        });
+        b_mylots.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i_mylots = new Intent(userProfile.this, myLots.class);
+                startActivity(i_mylots);
+            }
+        });
     }
 }
