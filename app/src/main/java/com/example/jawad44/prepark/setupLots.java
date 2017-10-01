7ea95b0b363d71fc694191e5325be02641ba108e
jawad44 <jawad44@iastate.edu>
2017-09-30 23:02:24 -0500

    probably fixed the screen

--- app/src/main/java/com/example/jawad44/prepark/setupLots.java
+++ app/src/main/java/com/example/jawad44/prepark/setupLots.java
@@ -1,7 +1,10 @@
 package com.example.jawad44.prepark;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class setupLots extends AppCompatActivity {
 
@@ -10,4 +13,9 @@ public class setupLots extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup_lots);
     }
+    final Button b_submitSL = (Button) findViewById(R.id.button_setuplots);
+    final Button b_saveSL = (Button) findViewById(R.id.button_save_setuplots);
+
+
+
 }
