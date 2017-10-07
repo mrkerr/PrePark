210e73e8f77d1cb03692a4798d794fae06d6198e
jawad44 <jawad44@iastate.edu>
2017-10-06 21:33:05 -0500

    added server related files

---
+++ PrePark/app/src/main/java/matt/prepark/LotRequest.java
@@ -0,0 +1,37 @@
+package matt.prepark;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by jawad44 on 10/6/17.
+ */
+
+public class LotRequest extends StringRequest{
+    private static final String REGISTER_REQUEST_URL = "http://proj-309-sb-b-2.cs.iastate.edu/lotsetup.php";
+    private Map<String, String> params;
+
+    public RegisterRequest(String address, String city, String state, String zip, String spots, String time, String rate, Response.Listener<String> listener) {
+        super(Request.Method.POST, REGISTER_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("address", address);
+        params.put("address", city);
+        params.put("address", state);
+        params.put("address", zip);
+        params.put("address", spots);
+        params.put("address", time);
+        params.put("address", rate);
+        params.put("username", username);
+        params.put("password", password);
+        params.put("email", email);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
