adb9cc98b8f80ef43e468dcc350fb5cc2265f335
jawad44 <jawad44@iastate.edu>
2017-10-15 16:48:21 -0500

    new screen

---
+++ PrePark/app/src/main/java/matt/prepark/BuySpot.java
@@ -0,0 +1,13 @@
+package matt.prepark;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BuySpot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy_spot);
+    }
+}
