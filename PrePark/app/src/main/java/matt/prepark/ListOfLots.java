e5cb728ea4f294eb5e67c429a926a5a3df4c13d6
Mitchell Kerr <mrkerr@iastate.edu>
2017-10-24 17:28:48 -0500

    List view looks better now, thanks Ridwan

--- PrePark/app/src/main/java/matt/prepark/ListOfLots.java
+++ PrePark/app/src/main/java/matt/prepark/ListOfLots.java
@@ -26,24 +26,17 @@ public class ListOfLots extends AppCompatActivity {
         setContentView(R.layout.activity_list_of_lots);
         listView = findViewById(R.id.list_view);
 
-        list.add("Mitch");
-        list.add("Matt");
-        list.add("Jawad");
-        list.add("Ridwan");
-        list.add("Iowa State");
-        list.add("ECpE");
-        list.add("Mitch");
-        list.add("Matt");
-        list.add("Jawad");
-        list.add("Ridwan");
-        list.add("Iowa State");
-        list.add("ECpE");
-        list.add("Mitch");
-        list.add("Matt");
-        list.add("Jawad");
-        list.add("Ridwan");
-        list.add("Iowa State");
-        list.add("ECpE");
+        String[] listofLots = {"Iowa State University, Ames IA", "1305 Georgia Avenue Ames, IA", "4800 Mortensen Rd Ames IA", "Jack Trice Stadium Ames, IA"
+        + "123 Main Street Ames, IA", "Iowa State University, Ames IA", "1305 Georgia Avenue Ames, IA", "4800 Mortensen Rd Ames IA", "Jack Trice Stadium Ames, IA"
+                + " 123 Main Street Ames, IA", "Iowa State University, Ames IA", "1305 Georgia Avenue Ames, IA", "4800 Mortensen Rd Ames IA", "Jack Trice Stadium Ames, IA"
+                + " 123 Main Street Ames, IA", "Iowa State University, Ames IA", "1305 Georgia Avenue Ames, IA", "4800 Mortensen Rd Ames IA", "Jack Trice Stadium Ames, IA"
+                + " 123 Main Street Ames, IA", "Iowa State University, Ames IA", "1305 Georgia Avenue Ames, IA", "4800 Mortensen Rd Ames IA", "Jack Trice Stadium Ames, IA"
+                + " 123 Main Street Ames, IA"};
+
+        for(int i = 0; i < listofLots.length; i++){
+            list.add(listofLots[i]);
+        }
+
 
         adapter = new ArrayAdapter(ListOfLots.this, android.R.layout.simple_list_item_1, list);
         listView.setAdapter(adapter);
