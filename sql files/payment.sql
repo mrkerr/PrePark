10c1063ac3c31c7882105bae4ca0642f3838587e
Matt Lawlor <mattlawlor@mattmacbook-pro.student.iastate.edu>
2017-11-05 16:01:52 -0600

    now can update map

--- sql files/payment.sql
+++ sql files/payment.sql
@@ -1,6 +1,8 @@
 create table payment_history(
-user varchar (32),
-payment varchar (100));
+buyer varchar (32),
+seller varchar (32),
+payment varchar (100),
+date date);
 
 insert into payment_history values ("Transaction: Price: 10.00 USD Location: Blaze Restaurant Ames IA Date: 10/05/17");
 
@@ -13,3 +15,5 @@ drop table payment_history;
 select * from payment_history where user = "Matt";
 
 insert into payment_history values ("Matt", "lot 23", '1997-02-28');
+
+insert into payment_history values ("Matt", "Ridwan", "lot 21", '1997-02-28');
