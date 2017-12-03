894209bb17feb812250e0f1388fb36b11cbf94e6
jawad44 <jawad44@iastate.edu>
2017-12-03 16:10:13 -0600

    online offline status

---
+++ PrePark/app/src/main/java/matt/prepark/networkReceiver.java
@@ -0,0 +1,27 @@
+package matt.prepark;
+
+/**
+ * Created by jawad44 on 12/3/17.
+ */
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import static android.content.Context.CONNECTIVITY_SERVICE;
+public class networkReceiver extends BroadcastReceiver{
+    public static final String NETWORK_AVAILABLE = "Network Available";
+    public static final String IS_NETWORK_AVAILABLE = "isNetworkAvailable";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent networkStateIntent = new Intent(NETWORK_AVAILABLE);
+        networkStateIntent.putExtra(IS_NETWORK_AVAILABLE,  isConnectedToInternet(context));
+        LocalBroadcastManager.getInstance(context).sendBroadcast(networkStateIntent);
+
+    }
+
+}
